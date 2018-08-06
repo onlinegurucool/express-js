8fd6868addc6b317f9f18d2743ffb0339bdff9bb
Pratap Rockerss <pratap@techcetra.com>
2018-08-06 06:20:00 +0530

    express parameters done

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,10 @@ const
     express = require('express'),
     router = express.Router()
 
-router.get('/',function(req,res,next){
-    res.render('index', { title: 'Product' });
-})
-
-router.get('/category',function(req,res,next){
-    res.render('index', { title: 'Product category' });
-})
+router.get('/',
+    function(req,res,next){
+        res.send('next function called')
+    }
+)
 
 module.exports = router;
